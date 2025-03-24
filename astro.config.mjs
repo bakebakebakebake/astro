41d0c8e2b7fe85e05def89f298782c36e349b43f
bakebakebakebake <1523232694@qq.com>
2025-03-24 14:45:00 +0800

    修改了一下页面且修改了主题

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+import starlightThemeRapide from 'starlight-theme-rapide'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        starlight({
+            plugins: [starlightThemeRapide()],
+            title: 'My Docs',
+        }),
+    ],
+})
